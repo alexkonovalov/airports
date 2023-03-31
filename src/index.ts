bda8a5a1b73281b9eb0e74461ec97547bf25ce3b
Alex <alvkonovalov@gmail.com>
2023-03-31 16:08:29 +0300

    add distance measuring and setting

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import fs from "fs";
 import csv from "csv-parser";
 import { alg, Edge, Graph } from "graphlib";
 import { dijkstra } from "./dijkstra";
+import { getDistance, getPreciseDistance } from "geolib";
 // create a new graph
 const graph = new Graph();
 
-interface RowData {
+interface RouteRowData {
     "source airport": string;
     "destination airport": string;
+}
+
+interface AirportRowData {
+    IATA: string;
+    Latitude: string;
+    Longitude: string;
 }
 
 interface DijkstraResult {
@@ -15,46 +22,76 @@ interface DijkstraResult {
     previous: { [key: string]: string | null };
 }
 
-// read the CSV file and add nodes and edges to the graph
-fs.createReadStream("routes.csv")
+const airports: Record<string, { latitude: string; longitude: string }> = {};
+
+fs.createReadStream("airports.csv")
     .pipe(
         csv({
-            mapHeaders: ({ header }) => header.trim(),
+            //  separator: ";",
         })
     )
-    .on("data", (row: RowData) => {
-        const source = row["source airport"];
-        const destination = row["destination airport"];
-
-        // add source and destination airports as nodes, if not already present
-        if (!graph.hasNode(source)) {
-            graph.setNode(source);
-        }
-        if (!graph.hasNode(destination)) {
-            graph.setNode(destination);
-        }
-
-        const dist =
-            [source, destination].includes("KZN") &&
-            [source, destination].includes("IST")
-                ? 9999
-                : 1;
-
-        console.log({ source, destination, dist });
-        // add edge with weight equal to 1
-        graph.setEdge(source, destination, dist);
+    .on("data", ({ IATA, Latitude, Longitude }: AirportRowData) => {
+        airports[IATA] = { latitude: Latitude, longitude: Longitude };
     })
     .on("end", () => {
-        // compute the shortest path from KZN to TLL using the custom Dijkstra's algorithm with a maximum of 3 hops
-        const maxHops = 3;
-        function weight(e: Edge) {
-            return graph.edge(e);
-        }
-        const results = dijkstra(graph, "KZN", weight, undefined, maxHops);
+        // read the CSV file and add nodes and edges to the graph
+        fs.createReadStream("routes.csv")
+            .pipe(
+                csv({
+                    mapHeaders: ({ header }) => header.trim(),
+                })
+            )
+            .on("data", (row: RouteRowData) => {
+                const source = row["source airport"];
+                const destination = row["destination airport"];
 
-        // print the shortest distance between KZN and TLL airports
-        console.log(
-            `Shortest distance from KZN to TLL (limited to ${maxHops} hops):`,
-            results["IST"]
-        );
+                // add source and destination airports as nodes, if not already present
+                if (!graph.hasNode(source)) {
+                    graph.setNode(source);
+                }
+                if (!graph.hasNode(destination)) {
+                    graph.setNode(destination);
+                }
+
+                const from = airports[source];
+                const to = airports[destination];
+
+                const distance =
+                    from && to
+                        ? getPreciseDistance(from, to)
+                        : Number.POSITIVE_INFINITY;
+
+                // const dist =
+                //     [source, destination].includes("KZN") &&
+                //     [source, destination].includes("IST")
+                //         ? 9999
+                //         : 1;
+
+                // console.log({ source, destination, dd });
+                // add edge with weight equal to 1
+                graph.setEdge(source, destination, distance);
+            })
+            .on("end", () => {
+                // compute the shortest path from KZN to TLL using the custom Dijkstra's algorithm with a maximum of 3 hops
+                const maxHops = 2;
+                function weight(e: Edge) {
+                    return graph.edge(e);
+                }
+
+                const results = dijkstra(
+                    graph,
+                    "TLL",
+                    weight,
+                    undefined,
+                    maxHops
+                );
+
+                // print the shortest distance between KZN and TLL airports
+                console.log(
+                    `Shortest distance (limited to ${maxHops} hops):`,
+                    results["PEK"]
+                );
+
+                // console.log(airports["TLL"]);
+            });
     });
