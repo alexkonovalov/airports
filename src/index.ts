f2fb06186fee39ea22ee085691a81d95aaddc55c
Alex <alvkonovalov@gmail.com>
2023-04-05 16:34:38 +0300

    remove unused deps, add server listener

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,65 @@
-import { readFileSync } from "fs";
-import { Edge, json, alg } from "graphlib";
+import fs from "fs";
+import express, { Request, Response } from "express";
+import { Edge, json as graphlibJson } from "graphlib";
 import { dijkstra } from "./dijkstra";
 import { GRAPH_PATH } from "./constants";
 
-const graphObj = JSON.parse(readFileSync(GRAPH_PATH).toString());
-const graph = json.read(graphObj);
-const maxLegs = 4;
-
-function weight(e: Edge) {
+function weightFn(e: Edge) {
     const edge = graph.edge(e);
     return edge.distance;
 }
-const startTime = performance.now();
-const result = dijkstra({
-    g: graph,
-    source: "TLL",
-    target: "PEK",
-    weightFn: weight,
-    edgeFn: undefined,
-    isDeepEdgeFn: (e) => {
-        const edge = graph.edge(e);
-        return edge.isAir;
-    },
-    maxDepth: maxLegs,
+
+function isDeepEdgeFn(e: Edge) {
+    const edge = graph.edge(e);
+    return edge.isAir;
+}
+
+const MAX_LEGS = 4;
+const IATA_REGEX = /^[A-Z]{3}$/;
+
+const graphObj = JSON.parse(fs.readFileSync(GRAPH_PATH).toString());
+const graph = graphlibJson.read(graphObj);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get(
+    "/calculate",
+    (
+        { query }: Request<{}, {}, {}, { source?: string; target?: string }>,
+        res: Response
+    ) => {
+        if (!query.source || !query.target) {
+            res.status(400).send("Missing source or target airport IATA");
+            return;
+        }
+        const { source, target } = {
+            source: query.source.toUpperCase(),
+            target: query.target.toUpperCase(),
+        };
+
+        if (!IATA_REGEX.test(source) || !graph.hasNode(source)) {
+            res.status(403).send("Invalid source airport IATA");
+            return;
+        }
+        if (!IATA_REGEX.test(target) || !graph.hasNode(target)) {
+            res.status(403).send("Invalid target airport IATA");
+            return;
+        }
+
+        const result = dijkstra({
+            g: graph,
+            source,
+            target,
+            weightFn,
+            isDeepEdgeFn,
+            maxDepth: MAX_LEGS,
+        });
+
+        res.json(result);
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-const endTime = performance.now();
-const executionTime = endTime - startTime;
-
-console.log(`Execution time: ${executionTime} milliseconds`);
-
-console.log(`Shortest distance (limited to ${maxLegs} legs):`, result);
